d048f5e51c9f000f2b2b742c2ce8ae3ebccea055
Danylov <ol_danylov@ukr.net>
2022-07-19 12:01:16 +0300

    CueRotator realization.

---
+++ Assets/Scripts/CueControl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CueControl : MonoBehaviour
+{
+    private GameObject bitok;
+    private Transform cueTransform;
+    
+    private void Start()
+    {
+        cueTransform = GetComponent<Transform>();
+        bitok = GameObject.FindWithTag("Bitok");
+    }
+
+    private void SetBitokDirection()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/CueRotatorControl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class CueRotatorControl : MonoBehaviour
+{
+    private GameObject bitok;
+    private Transform cueRotatorTransform;
+    private CueControl cueControl; 
+
+    private void Start()
+    {
+        cueRotatorTransform = GetComponent<Transform>();
+        cueControl = GameObject.FindWithTag("CueStick").GetComponent<CueControl>();
+        bitok = GameObject.FindWithTag("Bitok");
+    }
+
+    private void Update()
+    {
+        // cueRotatorTransform = bitok.transform;
+        cueRotatorTransform.position = new Vector3(bitok.transform.position.x, bitok.transform.position.y, bitok.transform.position.z);
+        
+        Debug.Log("bitok.transform.position = " + bitok.transform.position); // Отладка
+        Debug.Log("transform.position = " + transform.position); // Отладка
+        
+    }
+}
